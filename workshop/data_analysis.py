586b4ae4a5351a95a8dcdaaf88b8673220a8185a
Itai Spigelman <itai10020@gmail.com>
2022-08-04 15:07:40 +0300

    data analysis finished

--- workshop/data_analysis.py
+++ workshop/data_analysis.py
@@ -27,17 +27,24 @@ coord_x, coord_y = np.meshgrid(coord, coord)
 V_xy = potential(coord_x, coord_y, a, C) # calculate potential
 
 plt.figure() # create empty figure
+plt.grid()
 plt.pcolormesh(coord_x, coord_y, V_xy) # create mesh color plot
 plt.colorbar() # add color bar
-plt.contour(coord_x, coord_y, V_xy, np.sort([-1 ,-0.75, -0.6,  -0.5, -0.25,  0 , 0.25, 0.5, 0.75,  1]), cmap='hot') 
+plt.contour(coord_x, coord_y, V_xy, np.sort([-1 ,-0.75, -0.6,  -0.5, -0.25,  0 , 0.25, 0.5, 0.75,  1]), cmap='hot')
 #cmap changes the colormap to the hot preset
+plt.xlabel("x [m]")
+plt.ylabel("y [m]")
 plt.show() # show figure
 
 
 x = np.linspace(-L,L,N)
 V_x = potential(x, [0] * N, a, C)
 plt.figure()
+plt.grid()
 plt.plot(x,V_x,'.', label="calculated potential")
+plt.xlabel("x [m]")
+plt.ylabel("V [V]")
+plt.show()
 
 #%% capacitor
 def V_decay(t,a,b):
@@ -60,19 +67,27 @@ t = np.array(C_data['t'].values)
 V_C = np.array(C_data['V_C'].values)
 
 plt.figure()
-plt.plot(t,V_C)
+plt.grid()
+plt.plot(t,V_C,label="data")
 
-# curve fitting means creating a curve of certain type that is as close as possible to all points of a given data set.
-
+# curve fitting means fitting a curve of certain type that is as close as possible to all points of a given data set.
 
 fit2 = cfit(V_decay,C_data['t'], C_data["V_C"]) # needs to find a = V_0 and b = 1/tau
 
 plt.plot(t, V_decay(t,fit2[0][0],fit2[0][1]),label="fitted curve")
+plt.legend()
+plt.xlabel("t [sec]")
+plt.ylabel("V [V]")
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 plt.show()
 
 plt.figure()
 plt.grid()
 plt.plot(t,np.log(V_C))
+plt.xlabel("t [sec]")
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+plt.ylabel("log(V) [V]")
+plt.show()
 
 # log of V_C should be linear in time
 plt.figure()
@@ -81,6 +96,9 @@ t1 = 0
 t2 = 0.000047
 inds = (t > t1) & (t < t2)
 plt.plot(t[inds], np.log(V_C)[inds],'.', label="data")
+plt.xlabel("time [sec]")
+plt.ylabel("log(V) [V]")
+plt.show()
 
 # linear regression means fitting a linear curve to a set of data points.
 reg2 = linregress(t[inds], np.log(V_C[inds]))
@@ -92,17 +110,16 @@ C_data["int_V_R"] = scipy.integrate.cumtrapz(C_data["V_R"], x = t, initial = 0)
 int_V_R = np.array(C_data["int_V_R"].values)
 
 plt.figure()
-plt.plot(int_V_R,V_C)
+plt.plot(int_V_R,V_C, label='data')
 reg3 = linregress(int_V_R, V_C)
 C_meas = int_V_R /(R*V_C)
 
-
-plt.figure()
 plt.plot(C_data["int_V_R"], linearCurve(C_data["int_V_R"], reg3.slope, reg3.intercept), label='regression')
-plt.xlabel("integral of V_R")
-plt.ylabel("V_C")
+plt.xlabel("integral of V_R [V*sec]")
+plt.ylabel("V_C [V]")
 plt.legend()
 plt.grid()
+plt.show()
 
 #%% Ohm
 R1 = 5.48 #Ohm
@@ -135,8 +152,13 @@ Et = Energy(P_t(VR,IR),t)
 indices = (Et > 0.01) & (Et < 0.9)
 Et = Et[indices]
 Rt = Rt[indices]
-plt.plot(Et,Rt)
-reg = linregress(Et,Rt)
+plt.figure()
+plt.plot(Et, Rt)
+plt.xlabel("E [J]")
+plt.ylabel("R [Ohm]")
+plt.grid()
+plt.show()
+reg = linregress(Et, Rt)
 R0 = reg.intercept
 aC = reg.slope/R0
 
@@ -149,19 +171,22 @@ def max_flux_index(flux):
 h = np.array([30,24,18,14,8]) * 1e-2
 
 Ind_data = []
+plt.figure()
+plt.grid()
 for n in range(0,5):
     df = pd.read_csv('Trace %d.csv'%n, header = 1)
     df = df.rename(columns = {"Time (s)":"t", "1 (VOLT)":"ref", "2 (VOLT)":"signal"})
     Ind_data.append(df)
-    plt.plot(df["t"],df["ref"], label=f"ref  {h[n]}")
-    plt.plot(df["t"],df["signal"], label=f"signal  {h[n]}")
+    plt.plot(df["t"], df["ref"], label=f"ref  {h[n]}")
+    plt.plot(df["t"], df["signal"], label=f"signal  {h[n]}")
+    plt.xlabel("t [sec]")
+    plt.ylabel("V [sec]")
 plt.legend()
+plt.show()
 
 plt.figure()
-
-
+plt.grid()
 t_coil = np.array([])
-
 for n in range(0,5):
     df = Ind_data[n]
     ref_flux = flux(df["ref"],df["t"])
@@ -174,23 +199,22 @@ for n in range(0,5):
     plt.plot(df["t"], signal_flux , label = f"signal  {h[n]}")
     plt.plot(df["t"][signal_ind], signal_flux[signal_ind],"ro")
     t_coil = np.append(t_coil, df["t"][signal_ind] - df["t"][ref_ind])
-    plt.grid()
-    plt.xlabel(t)
-    plt.yable("flux")
-    
+    plt.xlabel("t [sec]")
+    plt.ylabel("flux [V*sec]")
 plt.legend()
+plt.show()
 
 plt.figure()
-plt.plot(t_coil,h/t_coil,"ro")
 y_err = np.sqrt( np.power(1e-3/t_coil,2) + np.power(1e-3 * h/(t_coil**2),2)  )
-plt.errorbar(t_coil,h/t_coil,y_err,1e-3)
+plt.errorbar(t_coil,h/t_coil,y_err,1e-3,"ro")
 reg = linregress(t_coil,h/t_coil)
 v0 = reg.intercept
 a = reg.slope * 2
 plt.plot(t_coil, reg.slope * t_coil + reg.intercept)
-plt.xlabel("t_coil")
-plt.ylabel("h/t_coil")
+plt.xlabel("t_coil [sec]")
+plt.ylabel("h/t_coil [m/s]")
 plt.grid()
+plt.show()
 R_squared = reg.rvalue ** 2
 
 chi_squared = scipy.stats.chisquare(h/t_coil, reg.slope * t_coil + reg.intercept).statistic
