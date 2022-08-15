f32a4a0205abe09c5448f02c6d36824bb2467458
Itai Spigelman <itai10020@gmail.com>
2022-08-15 11:20:53 +0300

    spectroscopy data

--- spectroscopy/analysis.py
+++ spectroscopy/analysis.py
@@ -15,9 +15,9 @@ from data import *
 # 2d*sin(theta_min/2)=n*lambda
 
 # same wave length, different order numbers
-merc_theta_min = unumpy.uarray([-64, -36, -17, 0, 17, 36, 64], 5)
+merc_theta_min = unumpy.uarray([48.735, 31.95, 15.885, 0, -15.795, -31.815, -48.735], 0.001)
 merc_theta_min = 2 * np.pi * merc_theta_min / 360
-n = unumpy.uarray(range(-3, 4, 1), 0)
+n = unumpy.uarray([3, 2, 1, 0, -1, -2, -3], 0)
 
 x = unumpy.sin(merc_theta_min / 2)
 y = n
@@ -36,8 +36,9 @@ d = ufloat(reg.slope, reg.stderr) * mercury_wavlen / 2
 #%% Helium spectrum with diffraction grating
 
 # different wavelength, order number +/-1
-hel_theta_min = unumpy.uarray([], 5)
-merc_theta_min = 2 * np.pi * merc_theta_min / 360
+# color order: -20.43 light red, -19.305 red, -16.965 orange, -14.49 green, -14.22 cyan/green, -13.635 blue, -12.915 purple, 12.96 purple, 13.59 blue, 14.22 green/cyan, 14.535 green, 17.01 orange, 19.35 red, 20.43 light red
+hel_theta_min = unumpy.uarray([-20.43, -19.305, -16.965, -14.49, -14.22, -13.635, -12.915, 12.96, 13.59, 14.22, 14.535, 17.01, 19.35, 20.43], 0.01)
+hel_theta_min = 2 * np.pi * hel_theta_min / 360
 
 hel_wavelen = 2 * d * unumpy.sin(hel_theta_min / 2)
 
@@ -48,19 +49,19 @@ exp_hel_wavelen = np.array([396.47, 402.62, 412.08, 414.38, 438.79,
 #%% prism calibration
 
 # different wavelength, order number +/-1
-hel_delta_min = unumpy.uarray([], 5)
+hel_delta_min = unumpy.uarray([-48.05, -47.565, -47.205, -47.07, -46.125, -45.54, -45.315], 0.001)
 hel_delta_min = 2 * np.pi * hel_delta_min / 360
 
-x = 1/(hel_wavelen**2)
+x = 1/(hel_wavelen[7:]**2)
 y = hel_delta_min
 
 # delta_min = B_tag+C_tag/(lambda**2)
-reg2 = linregress(x, y)
+reg2 = linregress(noms(x), noms(y))
 B_tag = ufloat(reg2.intercept, reg2.intercept_stderr)
 C_tag = ufloat(reg2.slope, reg2.stderr)
 
-plt.errorbar(x, y, xerr=devs(x), yerr=devs(y), fmt="ro", label="data")
-plt.plot(x, B_tag+C_tag*x, label="fit")
+plt.errorbar(noms(x), noms(y), xerr=devs(x), yerr=devs(y), fmt="ro", label="data")
+plt.plot(noms(x), B_tag.nominal_value+C_tag.nominal_value*noms(x), label="fit")
 plt.xlabel(r"$\frac{1}{\lambda^2}$ $[\frac{1}{m^2}]$")
 plt.ylabel(r"$\delta_min$ $[1]$")
 plt.grid()
@@ -68,9 +69,10 @@ plt.legend()
 plt.show()
 
 #%% hydrogen spectrum with prism
-
-hyd_delta_min = unumpy.uarray([], 5)
-hyd_wavelen = sqrt(C_tag/(hyd_delta_min - B_tag))
+# red, light blue, purple, deep purple
+hyd_delta_min = unumpy.uarray([-45.09, -46.8, -47.88, -48.555], 0.001)
+hyd_delta_min = 2 * np.pi * hyd_delta_min / 360
+hyd_wavelen = unumpy.sqrt((C_tag/(hyd_delta_min - B_tag)))
 
 nu = c/hyd_wavelen
 e_deltas = h*nu
