05967729971d2d7de9998ea5b5b2b939ea6819d0
Itai Spigelman <itai10020@gmail.com>
2022-08-18 11:18:39 +0300

    minor dev adjustment

--- spectroscopy/analysis.py
+++ spectroscopy/analysis.py
@@ -14,7 +14,7 @@ from data import *
 # 2d*sin(theta_min/2)=n*lambda
 
 # same wave length, different order numbers
-merc_theta_min = unumpy.uarray([48.735, 31.95, 15.885, 0, -15.795, -31.815, -48.735], 0.001)
+merc_theta_min = unumpy.uarray([48.74, 31.95, 15.89, 0, -15.795, -31.82, -48.74], 0.01)
 merc_theta_min = 2 * np.pi * merc_theta_min / 360
 n = unumpy.uarray([3, 2, 1, 0, -1, -2, -3], 0)
 
@@ -37,7 +37,7 @@ d = ufloat(reg.slope, reg.stderr) * mercury_wavlen / 2
 # different wavelength, order number +/-1
 # color order: -20.43 light red, -19.305 red, -16.965 orange, -14.49 green, -14.22 cyan/green, -13.635 blue, -12.915 purple, 12.96 purple, 13.59 blue, 14.22 green/cyan, 14.535 green, 17.01 orange, 19.35 red, 20.43 light red
 hel_theta_min = unumpy.uarray(
-    [-20.43, -19.305, -16.965, -14.49, -14.22, -13.635, -12.915, 12.96, 13.59, 14.22, 14.535, 17.01, 19.35, 20.43],
+    [-20.43, -19.305, -16.965, -14.49, -14.22, -13.64, -12.92, 12.96, 13.59, 14.22, 14.54, 17.01, 19.35, 20.43],
     0.01)
 hel_theta_min = 2 * np.pi * hel_theta_min / 360
 
@@ -50,7 +50,7 @@ exp_hel_wavelen = np.array([396.47, 402.62, 412.08, 414.38, 438.79,
 # %% prism calibration
 
 # different wavelength, order number +/-1
-hel_delta_min = unumpy.uarray([-48.05, -47.565, -47.205, -47.07, -46.125, -45.54, -45.315], 0.001)
+hel_delta_min = unumpy.uarray([-48.05, -47.57, -47.21, -47.07, -46.13, -45.54, -45.32], 0.01)
 hel_delta_min = 2 * np.pi * hel_delta_min / 360
 hel_delta_min *= -1
 
@@ -72,7 +72,7 @@ plt.show()
 
 # %% hydrogen spectrum with prism
 # red, light blue, purple, deep purple
-hyd_delta_min = unumpy.uarray([-45.09, -46.8, -47.88, -48.555], 0.001)
+hyd_delta_min = unumpy.uarray([-45.09, -46.8, -47.88, -48.56], 0.01)
 hyd_delta_min = 2 * np.pi * hyd_delta_min / 360
 hyd_delta_min *= -1
 hyd_wavelen = unumpy.sqrt((C_tag / (hyd_delta_min - B_tag)))
