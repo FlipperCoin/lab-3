e7c8dcea95db1e05bd2a8fea33115612ee7f5a6c
Itai Spigelman <itai10020@gmail.com>
2022-08-20 20:21:11 +0300

    sound waves wip

--- sound_waves/analysis.py
+++ sound_waves/analysis.py
@@ -9,19 +9,19 @@ from scipy.constants import R, Avogadro, proton_mass, neutron_mass
 
 #%% part 0 - prep
 
-f = unumpy.uarray([3,  3.2, 3.5, 3.8, 4.1, 4.4, 4.7, 5, 5.3, 5.6, 5.9, 6.2], 0.1e-3)  # KHz
-A = unumpy.uarray([19, 20,  21.5,23.5, 26.5, 29, 29.7, 34, 25, 21.7, 20, 19.5 ], [4, 4, 4, 4, 2, 1, 1, 1, 1, 1, 2, 3 ])  # mV
+f = unumpy.uarray([3,  3.2, 3.5, 3.8, 4.1, 4.4, 4.7, 5, 5.3, 5.6, 5.9, 6.2], 1e-3)  # KHz
+A = unumpy.uarray([19, 20,  21.5, 23.5, 26.5, 29, 29.7, 34, 25, 21.7, 20, 19.5 ], [4, 4, 4, 4, 2, 1, 1, 1, 1, 1, 2, 3 ])  # mV
 
 plt.errorbar(noms(f), noms(A), xerr=devs(f), yerr=devs(A), fmt="bo", label="data")
 plt.grid()
-plt.xlabel(r"f $\left[ \frac{1}{sec} \right]$")
-plt.ylabel(r"A $\left[ m \right]$")
+plt.xlabel(r"f $\left[ KHz \right]$")
+plt.ylabel(r"A $\left[ mV \right]$")
 plt.show()
 
 res_idx = np.argmax(noms(A))
 f_res, A_res = f[res_idx], A[res_idx]
 
-f_res = 5000
+f_res = ufloat(5000, 1)
 
 #%% part 1 - sound phase velocity in air
 
@@ -29,7 +29,7 @@ x0 = 127e-2
 x = unumpy.uarray([127, 130.5, 134, 137.4, 140.7, 144.5, 147.9, 151.6, 155], 0.3)*1e-2
 x = x - x0
 phi = unumpy.uarray([0, 177, 360+0, 360+177, 720+0, 720+177, 1080+0, 1080+177, 360*4+0], [1.5, 0.5, 3, 5, 3, 2,  3, 5, 5])
-phi = unumpy.uarray(np.deg2rad(noms(phi)), devs(phi))
+phi = unumpy.uarray(np.deg2rad(noms(phi)), np.deg2rad(devs(phi)))
 reg = linregress(noms(x), noms(phi))
 
 plt.errorbar(noms(x), noms(phi), xerr=devs(x), yerr=devs(phi), fmt="bo", label="data")
@@ -55,59 +55,51 @@ gammaR_over_M = v_zero**2/T_zero
 v_air = sqrt(gammaR_over_M*(T_lab + 273.15))
 
 #%% part 2 - sound velocity in gases
-L = ufloat(0.98, 0.001)
+L = ufloat(0.98, 0.01)
 
 # helium
-m = 4e-3
-M = m
+M = 4e-3
 gamma = 1.66
 v_helium = sqrt(gamma*R*(T_lab + 273.15)/M)
 
 # CO2
-m = 44e-3
-M = m
+M = 44e-3
 gamma = 1.304
 v_co2 = sqrt(gamma*R*(T_lab + 273.15)/M)
 
 gas = {
     "air": {
         "v_theo": v_air,
-        "T_max": None,
-        "f_min": None
+        "T_min": None,
+        "f_max": None
     },
     "he": {
         "v_theo": v_helium,
-        "T_max": None,
-        "f_min": None
+        "T_min": None,
+        "f_max": None
     },
     "co2": {
         "v_theo": v_co2,
-        "T_max": None,
-        "f_min": None
+        "T_min": None,
+        "f_max": None
     }
 }
+
+# T_pipe - edge up of 4th peak
+gas["air"]["T_pipe"] = ufloat(19.90e-3, 0.02e-3)
+gas["he"]["T_pipe"] = ufloat(8.58e-3, 0.02e-3)
+gas["co2"]["T_pipe"] = ufloat(25.04e-3, 0.02e-3)
 
 # T <= 1/2*L/v, f >= 2*v/L
 for g in gas.keys():
     gas[g]["T_min"] = L/gas[g]["v_theo"]*4
     gas[g]["f_max"] = 1/gas[g]["T_min"]
+    gas[g]["v_meas"] = 7 * L / gas[g]["T_pipe"]
+    gas[g]["relative_err"] = gas[g]["v_meas"].std_dev / gas[g]["v_meas"].nominal_value
 
-# edge up of 4th peak
-T_air = ufloat(19.90e-3, 0.02e-3)
-T_co2 = ufloat(25.04e-3, 0.02e-3)
-T_he = ufloat(8.58e-3, 0.02e-3)
+gas["he"]["air_prop"] = (7*L/gas["he"]["v_theo"] - gas["he"]["T_pipe"]) / (7*L/gas["he"]["v_theo"] - 7*L/gas["air"]["v_theo"])
+gas["co2"]["air_prop"] = (7*L/gas["co2"]["v_theo"] - gas["co2"]["T_pipe"]) / (7*L/gas["co2"]["v_theo"] - 7*L/gas["air"]["v_theo"])
 
-gas["air"]["T_pipe"] = ufloat(0, 0)
-gas["air"]["T_mic"] = ufloat(0, 0)
-gas["air"]["v_meas"] = L/gas["air"]["T_pipe"]
-
-gas["he"]["T_pipe"] = ufloat(0, 0)
-gas["he"]["T_mic"] = ufloat(0, 0)
-gas["he"]["v_meas"] = L/gas["he"]["T_pipe"]
-
-gas["co2"]["T_pipe"] = ufloat(0, 0)
-gas["co2"]["T_mic"] = ufloat(0, 0)
-gas["co2"]["v_meas"] = L/gas["co2"]["T_pipe"]
 #
 # #%% part 3 - resonance
 # f_modes = unumpy.uarray([], 0)
