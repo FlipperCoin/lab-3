ad786f3b8ec32ef63435cb0e74a02c3859d88c44
Itai Spigelman <itai10020@gmail.com>
2022-08-22 10:48:09 +0300

    acoustic waves p2

--- sound_waves/analysis.py
+++ sound_waves/analysis.py
@@ -6,6 +6,7 @@ from uncertainties.umath import sqrt
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from scipy.constants import R, Avogadro, proton_mass, neutron_mass
+from scipy.optimize import curve_fit as cfit
 
 #%% part 0 - prep
 
@@ -102,13 +103,15 @@ gas["co2"]["air_prop"] = (7*L/gas["co2"]["v_theo"] - gas["co2"]["T_pipe"]) / (7*
 
 
 #%% part 3 - resonance
-f_modes = unumpy.uarray([], 0)
-n = np.array([])
 
-reg2 = linregress(n, noms(f))
-plt.errorbar(n, noms(f), yerr=devs(f), fmt="bo", label="data")
+T_lab2 = ufloat(21.9, 0.1)
+
+f_modes = unumpy.uarray([178.3, 354.7, 530.7, 709.8, 884.4, 1061, 1236, 1415, 1592, 1768, 1942, 2121, 2299, 2475, 2652], [0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+n = np.arange(1, len(f_modes)+1)
+
+reg2 = linregress(n, noms(f_modes))
+plt.errorbar(n, noms(f_modes), yerr=devs(f_modes), fmt="bo", label="data")
 plt.plot(n, reg2.intercept + reg2.slope*n, label="fit")
-plt.legend()
 plt.grid()
 plt.xlabel(r"n $\left[ 1 \right]$")
 plt.ylabel(r"f $\left[ \frac{1}{sec} \right]$")
@@ -118,47 +121,63 @@ v_air_meas_2 = 2*L*m2
 
 f_theory = n*v_air/2/L
 plt.errorbar(n, noms(f_theory), yerr=devs(f_theory), fmt="ro", label="theoretical velocity")
+plt.legend()
+plt.title("normal modes frequency vs mode number n")
 plt.show()
 
 #%% part 4 - standing waves
-L2 = ufloat(20e-2, 0)
-f2 = ufloat(0, 0)
+L2 = ufloat(246e-3, 1e-3)
+f2 = ufloat(4900, 1)
 
-x2 = unumpy.uarray([], 0)
-A2 = unumpy.uarray([], 0)
+lamb_n = 2/np.arange(1, 15)*L2
+f_n = gas["air"]["v_theo"]/lamb_n
 
-plt.errorbar(noms(x2), noms(A2), xerr=devs(x2), yerr=devs(A2), fmt="bo", label="data")
-plt.grid()
-plt.xlabel(r"x $\left[ m \right]$")
-plt.ylabel(r"A $\left[ mV \right]$")
-plt.show()
+chosen_frequency = 4900 #Hz
 
-standing_wavelen = ufloat(0, 0)
-standing_a = ufloat(0, 0)
-standing_phi = ufloat(0, 0)
+x2 = unumpy.uarray(np.arange(3, 25.5, 0.5), 0.1)*1e-2  # m
+A2 = unumpy.uarray([35, 40, 32, 24, 11, 6, 20, 33, 40, 37, 23, 10, 3, 20, 30, 39, 36, 26, 14, 9, 16, 30, 37, 35, 28, 16, 12, 17, 27, 36, 37, 30, 22, 13, 6, 21, 33, 35, 33, 26, 19, 20, 27, 34, 32],
+                   [2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 4, 1, 2, 4, 2, 3, 1, 2, 2, 1, 1, 3, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3])  # mV
 
-x = np.linspace(0, x2[len(x2)-1].nominal_value, 1000)
+x2 = x2[0:int(np.floor(len(x2)/2))]
+A2 = A2[0:int(np.floor(len(A2)/2))]
+
+# plt.errorbar(noms(x2), noms(A2), xerr=devs(x2), yerr=devs(A2), fmt="bo", label="data")
+# plt.grid()
+# plt.xlabel(r"x $\left[ m \right]$")
+# plt.ylabel(r"A $\left[ mV \right]$")
+# plt.show()
 
 def theo_curve(x, lamb, a, phi0):
-    return 2*a*np.cos(2*np.pi*(L2/(2*lamb))-phi0/2)*np.cos(np.pi/lamb*(2*x-L2)+phi0/2)
+    return 2*a*np.abs(np.cos(np.pi/lamb*(2*x-L2.nominal_value)+phi0/2))
+
+fit = cfit(theo_curve, noms(x2), noms(A2), p0=[0.08, 20, 0])
+
+standing_wavelen = ufloat(fit[0][0], np.sqrt(np.diag(fit[1]))[0])
+standing_a = ufloat(fit[0][1], np.sqrt(np.diag(fit[1]))[1])
+standing_phi = ufloat(fit[0][2], np.sqrt(np.diag(fit[1]))[2])
+
+x = np.linspace(0.03, x2[len(x2)-1].nominal_value, 1000)
 
 plt.errorbar(noms(x2), noms(A2), xerr=devs(x2), yerr=devs(A2), fmt="bo", label="data")
-plt.plot(x, theo_curve(x, standing_wavelen, standing_a, standing_phi), label="theoretical")
+plt.plot(x, noms(theo_curve(x, standing_wavelen.nominal_value, standing_a.nominal_value, standing_phi.nominal_value)), label="fit")
 plt.legend()
 plt.grid()
 plt.xlabel(r"x $\left[ m \right]$")
 plt.ylabel(r"A $\left[ mV \right]$")
+plt.title("2 mic, standing wave")
 plt.show()
 
 v_air_meas_3 = standing_wavelen*f2
 
-x3 = unumpy.uarray([], 0)
-A3 = unumpy.uarray([], 0)
+x3 = unumpy.uarray(np.arange(3, 25.5, 0.5), 0.1)*1e-2  # m
+A3 = unumpy.uarray([13, 39, 55, 60, 57, 49, 35, 8, 30.5, 57, 62.9, 60, 51.4, 34,  11, 28.5, 54, 59, 61, 53, 43, 23, 21, 53, 57, 55, 53, 39, 17, 15, 45, 58, 61, 55, 44, 23, 11, 41, 54, 58, 54, 46, 30, 8.5, 31],
+                   [1,  1,  2,  1,  2,  1,  1,  1, 1,    1,  0.2,  0.3,0.3,  0.5, 1,  1,    1,  2,  1,  2,  1,  1,  1,  1,  1,  1,  1,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0.5, 0.5])
 
 plt.errorbar(noms(x3), noms(A3), xerr=devs(x3), yerr=devs(A3), fmt="bo", label="data")
 plt.grid()
 plt.xlabel(r"x $\left[ m \right]$")
 plt.ylabel(r"A $\left[ mV \right]$")
+plt.title("1 mic, standing wave")
 plt.show()
 
 
